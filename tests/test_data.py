ae2e34cbbc53ae6d835f8cadac6c08e4efc39ff8
Max Meldal <70686398+maxmeldal@users.noreply.github.com>
2026-01-12 11:21:14 +0100

    actions

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,9 +1,14 @@
 from torch.utils.data import Dataset
 
-from template.data import MyDataset
+from template.data import corrupt_mnist
 
 
 def test_my_dataset():
     """Test the MyDataset class."""
-    dataset = MyDataset("data/raw")
-    assert isinstance(dataset, Dataset)
+    train, test = corrupt_mnist()
+    assert len(train) == 30000
+    assert len(test) == 5000
+    assert train[0][0].shape == (1, 28, 28)
+    assert test[0][0].shape == (1, 28, 28)
+    assert isinstance(train, Dataset)
+    assert isinstance(test, Dataset)
---
+++ tests/test_model.py
@@ -0,0 +1,15 @@
+from template.data import corrupt_mnist
+from template.model import MyAwesomeModel
+import torch
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+
+
+def test_model_output_shape():
+    """Test that the model output has the correct shape."""
+    model = MyAwesomeModel().to(DEVICE)
+    train_set, _ = corrupt_mnist()
+    sample_img, _ = train_set[0]
+    sample_img = sample_img.unsqueeze(0).to(DEVICE)  # Add batch dimension
+    output = model(sample_img)
+    assert output.shape == (1, 10) 
